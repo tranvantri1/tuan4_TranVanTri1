64fb62464aa45a37ec8841dfcc028ca7d110e014
tvtri12c4.c3thdao2019@gmail.com <tvtri12c4.c3thdao2019@gmail.com>
2022-03-23 18:12:58 +0700

    Add project files.

---
+++ Controllers/NguoiDungController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using tuan4_TranVanTri1.Models;
+
+namespace tuan4_TranVanTri1.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        MyDataDataContext data = new MyDataDataContext();
+        [HttpGet]
+
+        // GET: NguoiDung
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangKy(FormCollection collection, KhachHang kh)
+        {
+            var hoten = collection["ho ten"];
+            var tendangnhap = collection["tendangnhap"];
+            var matkhau = collection["matkhau"];
+            var MatKhauXacNhan = collection["MatKhauXacNhan"];
+            var email = collection["email"];
+            var diachi = collection["diachi"];
+            var dienthoai = collection["dienthoai"];
+            var ngaysinh = String.Format("{0:MM/dd/yyyy}", collection["ngaysinh"]);
+            if (String.IsNullOrEmpty(MatKhauXacNhan))
+            {
+                ViewData["NhapMKXN"] = "phải nhập mật khẩu xác nhận! ";
+            }
+            else
+            {
+                if (!matkhau.Equals(MatKhauXacNhan))
+                {
+                    ViewData["MatKhauGiongNhau"] = "Mật khẩu và mật khẩu xác nhận phải giống nhau";
+
+                }
+                else
+                {
+                    kh.hoten = hoten;
+                    kh.tendangnhap = tendangnhap;
+                    kh.matkhau = matkhau;
+                    kh.email = email;
+                    kh.diachi = diachi;
+                    kh.dienthoai = dienthoai;
+                    kh.ngaysinh = DateTime.Parse(ngaysinh);
+                    data.KhachHangs.InsertOnSubmit(kh);
+                    data.SubmitChanges();
+                    return RedirectToAction("DangNhap");
+                }
+            }
+            return this.DangKy();
+        }
+        [HttpGet]
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection collection)
+        {
+            var tendangnhap = collection["tendangnhap"];
+            var matkhau = collection["matkhau"];
+            KhachHang kh = data.KhachHangs.SingleOrDefault(n => n.tendangnhap == tendangnhap && n.matkhau == matkhau);
+            if (kh != null)
+            {
+                ViewBag.ThongBao = "Chuc Mung Dang Nhap Thanh Cong";
+                Session["TaiKhoan"] = kh;
+            }
+            else
+            {
+                ViewBag.ThongBao = "Ten Dang Nhap Hoac Mat Khau Khong Dung";
+
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("tuan4_TranVanTri1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("tuan4_TranVanTri1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("59c02064-db4a-4d1e-ab0e-51f754d9dee5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
